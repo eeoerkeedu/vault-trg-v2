d5b8954c990fd925557ade81904bd2f2b15ec54a
LilTzar <eeoerkeedu@gmail.com>
2022-08-23 11:21:03 -0600

    added styled components to packages and added test elements

--- vault-app/components/layout.js
+++ vault-app/components/layout.js
@@ -10,19 +10,9 @@ export default function Layout({ children, home }) {
 	return (
 		<div className={styles.container}>
 			<Head>
-				<link rel="icon" href="/favicon.ico" />
-				<meta
-					name="description"
-					content="Learn how to build a personal website using Next.js"
-				/>
-				<meta
-					property="og:image"
-					content={`https://og-image.vercel.app/${encodeURI(
-						siteTitle
-					)}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-				/>
+				<link rel="icon" href="/vault-favicon.png" />
+				<meta name="description" content="The official Vault ttrpg website." />
 				<meta name="og:title" content={siteTitle} />
-				<meta name="twitter:card" content="summary_large_image" />
 			</Head>
 			<header className={styles.header}>{home ? <></> : <></>}</header>
 			<main>{children}</main>
--- vault-app/pages/index.js
+++ vault-app/pages/index.js
@@ -2,22 +2,38 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
+import logo from "../public/VaultLogoforsite.png";
 import Layout, { siteTitle } from "../components/layout";
+import styled, { css } from "styled-components";
+
+const Button = styled.button`
+	background: transparent;
+	border-radius: 3px;
+	border: 2px solid palevioletred;
+	color: palevioletred;
+	margin: 0 1em;
+	padding: 0.25em 1em;
+
+	${(props) =>
+		props.primary &&
+		css`
+			background: palevioletred;
+			color: white;
+		`};
+`;
 
 export default function Home() {
 	return (
 		<Layout home>
-			<Head>
-				<title>{siteTitle}</title>
-				<link rel="icon" href="/vault-favicon.png" />
-			</Head>
-
 			<section className="">
+				<Image src={logo} id="logo"></Image>
 				<p>[Your Self Introduction]</p>
 				<p>
 					(This is a sample website - you’ll be building a site like this on{" "}
 					<a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
 				</p>
+				<Button>I'm a button</Button>
+				<Button primary>Prime button</Button>
 			</section>
 		</Layout>
 	);
